08e32a9b2fe15066d154f617973bcdba9fdebef9
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-09-11 23:28:58 +0700

    add _document and edit import

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class CustomDocument extends Document {
+  return = (): JSX.Element => (
+    <Html>
+      <Head></Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default CustomDocument;
