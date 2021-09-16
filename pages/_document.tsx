12fa39e8aa69a3ee01202e1c8bf9a117a613d7be
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-09-17 00:47:49 +0700

    add grand design to tailwind config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,27 @@
 // eslint-disable-next-line @next/next/no-document-import-in-page
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import React, { ReactElement } from "react";
 
-class CustomDocument extends Document {
-  return = (): JSX.Element => (
-    <Html>
-      <Head></Head>
-      <body>
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render(): ReactElement {
+    return (
+      <Html lang="en">
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
+      <body className="font-quicksand">
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+    )
+  }
 }
 
-export default CustomDocument;
+export default MyDocument
