c83d47bd9904e54049fefaa6f1bb51aabd011b83
graciellavl <graciellavl@users.noreply.github.com>
2021-09-17 21:38:18 +0700

    add responsive in the text

--- app/components/templates/HomePage/HomePage.tsx
+++ app/components/templates/HomePage/HomePage.tsx
@@ -51,7 +51,7 @@ const Home: NextPage = () => {
           videoStyle={styles.videoStyle}
         />
       </div>
-      <div className={"my-24 text-h1 text-center"}>
+      <div className={"my-24 text-center text-h3 md:text-h1"}>
         {" "}
         and <b>implement</b> it{" "}
       </div>
