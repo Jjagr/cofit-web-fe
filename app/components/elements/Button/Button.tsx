382f45246fa3b8d958be1388b9008dc81747f0dc
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-09-17 21:33:28 +0700

    minor change button

--- app/components/elements/Button/Button.tsx
+++ app/components/elements/Button/Button.tsx
@@ -6,18 +6,19 @@ type Props = {
     children: React.ReactNode,
     onClick: MouseEventHandler
     className?: string,
+    rounded: "rounded-24" | "rounded-10"
 }
 
-const Button = ({ color, children, className, onClick }: Props) => {
+const Button = ({ color, children, className, onClick, rounded }: Props) => {
     return (
         <div onClick={onClick} className={
             color === "gray" 
-            ? `${className} ${styles.button} bg-primary-700 text-primary-50` 
+            ? `${className} ${styles.button} lg:${rounded} rounded-10 bg-primary-700 text-primary-50` 
             : color === "black"
-            ? `${className} ${styles.button} bg-black text-primary-50`
+            ? `${className} ${styles.button} lg:${rounded} rounded-10 bg-black text-primary-50`
             : color === "white"
-            ? `${className} ${styles.button} bg-primary-50`
-            : `${className} ${styles.button}`}>
+            ? `${className} ${styles.button} lg:${rounded} rounded-10 bg-primary-50`
+            : `${className} ${styles.button} lg:rounded-24 rounded-12`}>
             {children}
         </div>
     )
