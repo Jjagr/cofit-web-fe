5617b70b32beae0ca317c94743db0d902ea8bba0
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-09-18 02:08:04 +0700

    fix tag

--- app/components/elements/Tag/Tag.tsx
+++ app/components/elements/Tag/Tag.tsx
@@ -2,7 +2,8 @@ import styles from './Tag.module.css'
 
 export enum TagColor {
     ORANGE="bg-secondary-orange",
-    YELLOW="bg-secondary-yellow"
+    YELLOW="bg-secondary-yellow",
+    GRAY="bg-primary-200",
 }
 
 type Props = {
@@ -12,7 +13,7 @@ type Props = {
 
 const Tag = ({text, color}: Props) => {
     return (
-        <div className={[styles.tag,color].join(" ")} >
+        <div className={`${styles.tag} ${color} ${color===TagColor.GRAY?"text-primary-500":"text-primary-50"}`} >
             {text}
         </div>
     )
