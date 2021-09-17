f9e0c3b90ef20c49ea1525350b31b2ad01d388bd
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-09-17 17:42:35 +0700

    fix import constant

--- app/components/templates/HomePage/HomePage.tsx
+++ app/components/templates/HomePage/HomePage.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 import { Presets, Align, Weight } from 'types/TextStyles'
 import HeadText from '@element/HeadText/HeadText'
-import BodyText from '@element/BodyText/BodyText'
+import ProfileSrc, {PostType} from '@module/ProfileSrc/ProfileSrc'
+import Video from '@element/Video/Video'
+import { DATA, FEATURE } from '@constant/index'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from './Home.module.css'
-import Video from '@element/Video/Video'
 import ReactPlayer from 'react-player'
 
 const Home: NextPage = () => {
@@ -15,6 +16,12 @@ const Home: NextPage = () => {
         <Video overlay link={'assets/video/cofit-intro.mp4'} autoPlay style={"h-screen w-screen"} videoStyle={styles.videoStyle}/>
       </div>
       <div className={"h-96"}/>
+      <ProfileSrc
+        profile={DATA[0].user}
+        icon={true}
+        postType={PostType.POST}
+        time={DATA[0].time}
+      />
     </div>
   );
 };
---
+++ app/constants/index.ts
@@ -0,0 +1,58 @@
+import {ActType} from 'types/Activity';
+
+export type Activity = {
+    type: ActType
+    user: string
+    title: string
+    time: Date
+    helpful: number
+    comment: number
+}
+
+export const DATA: Array<Activity>  = [
+    {
+        type: ActType.MealPlan,
+        user: "Jacelyn Felisha",
+        title: "My 7 Days Diet Meal Plan",
+        time: new Date("September 17, 2021 15:27:00"),
+        helpful: 50,
+        comment: 10
+    },
+    {
+        type: ActType.MealPlan,
+        user: "Jacelyn Felisha",
+        title: "My 7 Days Diet Meal Plan",
+        time: new Date("September 15, 2021 07:00:00"),
+        helpful: 50,
+        comment: 10
+    },
+    {
+        type: ActType.MealPlan,
+        user: "Jacelyn Felisha",
+        title: "My 7 Days Diet Meal Plan",
+        time: new Date("September 15, 2021 07:00:00"),
+        helpful: 50,
+        comment: 10
+    },
+];
+
+export const FEATURE = [
+    {
+      image: "/image/move.png",
+      link: "/",
+      title: "FitMove",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur viverra posuere sed vel commodo et. Pharetra ut tellus scelerisque nunc vitae quam et eleifend. Vitae varius orci aenean congue in nulla augue. Neque nullam dignissim orci turpis. Mi facilisi egestas sit sapien mi.",
+    },
+    {
+      image: "/image/eat.png",
+      link: "/",
+      title: "FitEat",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur viverra posuere sed vel commodo et. Pharetra ut tellus scelerisque nunc vitae quam et eleifend. Vitae varius orci aenean congue in nulla augue. Neque nullam dignissim orci turpis. Mi facilisi egestas sit sapien mi.",
+    },
+    {
+      image: "/image/log.png",
+      link: "/",
+      title: "FitLog",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur viverra posuere sed vel commodo et. Pharetra ut tellus scelerisque nunc vitae quam et eleifend. Vitae varius orci aenean congue in nulla augue. Neque nullam dignissim orci turpis. Mi facilisi egestas sit sapien mi.",
+    },
+  ];
