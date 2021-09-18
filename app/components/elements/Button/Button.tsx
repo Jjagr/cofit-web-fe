da303020b1696eb302c798ef2ffee41037a91700
Renaldi Arlin <johanes123renaldi@gmail.com>
2021-09-18 09:53:41 +0700

    Changed navbar, favicon, and coming-soon

--- app/components/elements/Button/Button.tsx
+++ app/components/elements/Button/Button.tsx
@@ -18,7 +18,7 @@ const Button = ({ color, children, className, onClick, rounded }: Props) => {
             ? `${className} ${styles.button} lg:${rounded} rounded-10 bg-black text-primary-50`
             : color === "white"
             ? `${className} ${styles.button} lg:${rounded} rounded-10 bg-primary-50`
-            : `${className} ${styles.button} lg:${rounded} rounded-10`}>
+            : `${className} ${styles.button} lg:${rounded} rounded-10 hover:shadow-none`}>
             {children}
         </div>
     )
