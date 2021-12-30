e0a2f93b89299ae08fced39bd572469231a1c59e
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-12-30 15:26:15 +0700

    change background color

--- app/components/modules/FeatureCard/FeatureCard.tsx
+++ app/components/modules/FeatureCard/FeatureCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./FeatureCard.module.css";
 import Image from "next/image";
 import "tailwindcss/tailwind.css";
@@ -17,18 +17,17 @@ const FeatureCard = ({
   image,
   alignText,
 }: Props) => {
-  const [isHover, setIsHover] = useState(false);
   const leftAlign: boolean = alignText==='left';
 
   return (
     <div className={`${styles.cardContainer} flex-col-reverse ${leftAlign ? "xl:flex-row" : "xl:flex-row-reverse"}`}>
-      <div className={`${styles.cardText} ${leftAlign ? "xl:text-left" : "xl:text-right"} text-center`}>
+      <div className={`${styles.cardText} flex flex-col ${leftAlign ? "xl:items-start xl:text-left" : "xl:items-end xl:text-right"} text-center`}>
         <p className={`${styles.cardTitle}`}>{title}</p>
         <div className={`${styles.cardCaption} xl:mb-8`}>
           {caption}
         </div>
         <Button
-          color="none"
+          color="white"
           className="xl:w-[235px] bg-orange-gradient text-primary-50 font-bold xs:rounded-24"
           onClick={() => {}}
           rounded="rounded-24"
